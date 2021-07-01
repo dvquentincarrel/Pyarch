18576062d2a24347b87bc3c0350b884e09c3cbb4
Quentin Carrel <dvquentincarrel@gmail.com>
2021-07-01 11:14:20 +0200

    removed cross-import, added calls for get_tags_of_id (fetch_tags), filter_ids_by_tag (fetch_ids)

--- arch_con.py
+++ arch_con.py
@@ -1,3 +1,10 @@
-
 import arch_model as model
-import arch_UI as view
+
+def get_tags_of_id(id : str) -> [str]:
+    return model.fetch_tags(id)
+
+def filter_ids_by_tag(tag : str) -> [str]:
+    return model.fetch_ids(tag)
+
+def undo_last_transfer() -> None:
+    pass
